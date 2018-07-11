2d383e661808fb2dcd1c356075e1664d91f3d064
Arfan Fudyartanto <arfan.fudyartanto@gmail.com>
2018-07-12 04:43:53 +0700

    initial commit

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+FORCE=false
+DOCKER_NAME=alpine-concrete5
+
+# parsing parameter
+while [ "$1" != "" ]; do
+  if [ $1 == "-f" ]; then 
+    FORCE=true
+  fi
+  shift
+done
+
+# if any exist docker container then stop it
+if docker ps -a | grep $DOCKER_NAME | grep Up; then
+  docker stop $DOCKER_NAME
+fi
+
+# if any exists docker container then remove it
+if docker ps -a | grep $DOCKER_NAME; then
+  docker rm $DOCKER_NAME
+fi
+
+# docker image exists and force then remove existing image
+if docker images | grep fudyartanto/$DOCKER_NAME && $FORCE; then
+  docker rmi fudyartanto/$DOCKER_NAME
+fi
+
+# not docker image exists or force then build docker
+if ! docker images | grep fudyartanto/$DOCKER_NAME || $FORCE; then
+  docker build -t fudyartanto/$DOCKER_NAME .
+fi
+
+# docker run -tid -p 8080:80 --name $DOCKER_NAME fudyartanto/$DOCKER_NAME
